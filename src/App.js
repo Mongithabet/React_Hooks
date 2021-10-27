0c71311b9142616a0da50a66b9852dc9bfb98fa6
Mongithabet <stechnopc@yahoo.fr>
2021-10-27 17:33:09 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ClassState from './components/ClassState';
+import FunctionState from './components/FunctionState';
+function App() {
+  return (
+    <div className="App">
+     <h1 className="text-center">useState Hooks</h1>
+     <ClassState/>
+     <hr />
+     <FunctionState/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FunctionState.js
@@ -0,0 +1,14 @@
+import React,{useState} from 'react'
+
+
+const FunctionState = () =>{
+   const [counter,setCounter] = useState(0)
+  
+    return(
+        <div>
+            <p>FonctionState : {counter}</p>
+         <button onClick={()=>setCounter(prevCounter =>prevCounter+1)}>State dans Function: </button>   
+        </div>
+    )
+}
+export default FunctionState
